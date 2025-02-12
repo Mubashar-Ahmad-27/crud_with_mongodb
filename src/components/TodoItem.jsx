960e3e25cf16f9250aa7ac4d011af48f946f10f7
Mubashar Ahmad <iammubashar27@gmail.com>
2025-02-12 08:58:43 -0800

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -15,7 +15,7 @@ export default function TodoItem({ task, deleteTask }) {
 
     return (
         <li className="flex justify-between items-center bg-gray-700 text-white px-4 py-2 rounded-lg shadow-md">
-            <span className="text-lg">{task.title}</span>
+            <span className="text-lg text-white">{task.title}</span>
             <button  onClick={() => handleDelete(task.id)} 
                 className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 transition" >
                 Delete
